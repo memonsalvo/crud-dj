dd58d93eb94def38d6f54ff9d0167f00b54cbb76
memonsalvo <miguelmonsalvo19@gmail.com>
2023-10-18 06:13:57 -0500

    formulario a medias

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+#
 def helloworld(request):
-    return HttpResponse('hello world')
+    return render(request, 'signup.html')
