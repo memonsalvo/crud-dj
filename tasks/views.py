dcdaf835bbb4d6c7972d45350aea9699b17fc051
memonsalvo <miguelmonsalvo19@gmail.com>
2023-11-22 05:22:56 -0500

    registro usu

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 #
-def helloworld(request):
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+
+    if request.method == 'GET':
+        print('enviando formulario')
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # register user
+            try:
+                user = User.objects.create_user(username=request.POST['username']
+                    , password=request.POST['password1'])
+                user.save()
+                return HttpResponse('User created successfully')
+            except:
+                return HttpResponse('Username allready exist')
+            
+        return HttpResponse('Password do not match')
+        # print(request.POST)
+        # print('obteniendo datos')
+
     return render(request, 'signup.html',{
         'form': UserCreationForm
     })
