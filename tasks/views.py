b9018437fd52b2d74f3c0543d256c34453f7a0e5
memonsalvo <miguelmonsalvo19@gmail.com>
2023-11-29 04:20:59 -0500

    signin

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.contrib.auth import login, logout
-
+from django.contrib.auth import login, logout, authenticate
 
 #
+
+
 def home(request):
     return render(request, "home.html")
 
@@ -33,9 +34,31 @@ def signup(request):
             "form": UserCreationForm,
             "error": "Password do not match"})
 
+
 def tasks(request):
     return render(request, 'tasks.html')
+
 
 def signout(request):
     logout(request)
     return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'],
+            password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Username or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
+        
