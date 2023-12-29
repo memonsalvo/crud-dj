94e049a96bd97b47f958c8a320e291f4fbdd92bd
memonsalvo <miguelmonsalvo19@gmail.com>
2023-12-29 05:28:35 -0500

    actualizar

--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.contrib.auth import login, logout, authenticate
 from .forms import TaskForm
-from .models import Task #se llama para hacer una consula a la bd
+from .models import Task  # se llama para hacer una consula a la bd
 #
 
 
@@ -37,11 +37,13 @@ def signup(request):
 
 
 def tasks(request):
-    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True) #esto devuelve todas las tareas que estan en la bd
-    return render(request, 'tasks.html', {'tasks':tasks})
+    # esto devuelve todas las tareas que estan en la bd
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'tasks.html', {'tasks': tasks})
 # de all se pasa a filter para filtrar la info por usuario y que no se acceda a datos
-#de otros usuarios por medio de un usuario normal
-#dentro del filtro se busca la info correspondiente al usuario actual en inicio de sesion
+# de otros usuarios por medio de un usuario normal
+# dentro del filtro se busca la info correspondiente al usuario actual en inicio de sesion
+
 
 def create_task(request):
     if request.method == 'GET':
@@ -56,19 +58,33 @@ def create_task(request):
             new_task = form.save(commit=False)
             new_task.user = request.user  # es necesario llamar al usuario de la sesion por
             new_task.save()
-            return redirect('tasks')#al guardar redirecciona a la pag tasks
-        except ValueError:#se activa cuando consideremos un error
+            return redirect('tasks')  # al guardar redirecciona a la pag tasks
+        except ValueError:  # se activa cuando consideremos un error
             return render(request, 'create_task.html', {
                 'form': TaskForm,
-                'error':'please provide valida data'#mensaje de error sino funciona
+                'error': 'please provide valida data'  # mensaje de error sino funciona
             })
 
+
 def task_detail(request, task_id):
-    task = get_object_or_404(Task, pk=task_id)#para obtener un dato de primary key
-    return render(request, 'task_detail.html', {'task':task})#se llama el request para la peticion y 
-#asi mostrar la pagina en especifico de html que se quiere mostrar
-#en el diccionario se vincula la variable con el dato
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id)
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {'task': task, 'form': form})
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id)#condicional predeterminado de django
+            form = TaskForm(request.POST, instance=task)#va a recibir todos los datos que tiene post
+            form.save()#los datos dados antes se guardan y eso lo actualiza
+            return redirect('tasks')
+        except ValueError:#en caso de tener un error
+            return render(request, 'task_detail.html', {
+                'error':"Error updating task"})
+# asi mostrar la pagina en especifico de html que se quiere mostrar
+# en el diccionario se vincula la variable con el dato
 # es necesario al cambiar la forma de llamado llamar el modelo que se va a consultar
+
+
 def signout(request):
     logout(request)
     return redirect('home')
